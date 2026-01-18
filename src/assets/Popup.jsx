a2006394885984a3cdec7a4e4cd1da0f11771ff7
ashberkel <ashberkel@gmail.com>
2026-01-18 11:58:55 -0600

    fixed popup

--- src/assets/Popup.jsx
+++ src/assets/Popup.jsx
@@ -1,15 +1,33 @@
 import "./Popup.css";
+import { useEffect, useState } from "react";
 
 export default function Popup({ data, onClose }) {
+const [closing, setClosing] = useState(false);
+
+  useEffect(() => {
+    if (data) setClosing(false);
+  }, [data]);
+
   if (!data) return null;
 
+  const handleClose = () => setClosing(true);
+
   return (
-    <div className="popupOverlay" onClick={onClose}>
-      <div className="popup" onClick={e => e.stopPropagation()}>
+    <div
+      className={`popupOverlay ${closing ? "fadeOut" : "fadeIn"}`}
+      onAnimationEnd={() => {
+        if (closing) onClose();
+      }}
+    >
+      <div className="popup" onClick={(e) => e.stopPropagation()}>
+        <p id="exit" onClick={handleClose}>CLOSE</p>
         <h1>{data.h1Pop}</h1>
         <h2>{data.h2Pop}</h2>
         <p>{data.pPop}</p>
+      
       </div>
+      
     </div>
+    
   );
 }
